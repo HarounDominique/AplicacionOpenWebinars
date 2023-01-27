01956c3512cb2969e4da3beb0be05a046e54040f
HAROUN <haroundominique@gmail.com>
2023-01-27 17:56:04 +0100

    test commit

--- app/src/main/java/com/example/aplicacionopenwebinars/MainActivity.kt
+++ app/src/main/java/com/example/aplicacionopenwebinars/MainActivity.kt
@@ -3,53 +3,59 @@ package com.example.aplicacionopenwebinars
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.aplicacionopenwebinars.ui.theme.AplicacionOpenWebinarsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GreetingText(name = "World")
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Surface(
+        //modifier = Modifier.fillMaxSize(),
+        color = Color.LightGray
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            MySquare(miColor = Color.Black, tamanho = 80)
+            MySquare(miColor = Color.Green, tamanho = 180)
+
         }
     }
 }
 
 
 @Composable
-fun GreetingText(name: String) {
-    Text(
-        text = name,
-        style = MaterialTheme.typography.button
-        /*style = TextStyle(
-            color = Color.Blue,
-            fontStyle = FontStyle(1),
-            fontWeight = FontWeight.Bold,
-            fontSize = 108.sp
-        )
-        
-         */
-    )
+fun MySquare(miColor: Color, tamanho: Int) {
+    Surface(
+        color = miColor,
+        modifier = Modifier.size(tamanho.dp)
+    ) {
+
+    }
 }
 
 @Preview(showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     AplicacionOpenWebinarsTheme {
-        GreetingText(name = "World")
+        MainScreen()
     }
 }
